47632cea453e728d866cfd63bc2546f0ec550a57
Logan Miner <49819843+lrminer@users.noreply.github.com>
2020-11-25 09:46:47 -0500

    updated contact sizing

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -14,11 +14,11 @@ const Contact = () => {
       style={styles.wrapper}
     //   className="d-flex flex-column justify-content-around"
     >
-      <div className="pt-5 text-center display-1" id="phone">
+      <div className="pt-5 text-center" id="phone">
         <a href="tel:+16166487908">6166487908</a>
       </div>
-      <div className="pt-5 text-center display-1" id="email">
-        <a href="mailto:logan.miner@protonmail.com?subject=Portfolio">
+      <div className="pt-5 text-center" id="email">
+        <a href="mailto:logan.miner@protonmail.com?subject=lrminer.github.io">
           logan.miner@protonmail.com
         </a>
       </div>
