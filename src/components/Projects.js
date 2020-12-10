5a78f4dc6e82aeab65af27d4fb2eac005e231154
Logan Miner <49819843+lrminer@users.noreply.github.com>
2020-12-10 11:47:46 -0500

    refactor Projecs.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,26 @@
 import React from "react";
 
+const projects = [
+    {
+        title: "Pokemonline",
+        description: "Gen 1 Pokemon MMORPG powered by WebGL and socket.io",
+        href: "https://github.com/lrminer/pokemonline",
+    },
+    {
+        title: "Daily Translator",
+        description: "Full Catholic Bible with Spanish translations using swiping gestures. Pulls up a new chapter of bible each day.",
+        href: "https://github.com/lrminer/daily-translator",
+    }];
+
+const Project = props => {
+    return <div className="col-12 col-md-6 mt-5 mb-5">
+        <h2>{props.title}</h2>
+        <div className={'pt-3'}>{props.description}</div>
+        <div className={'pt-3'}><a href={props.href}>GitHub Repo</a>
+        </div>
+    </div>
+}
+
 const Projects = () => {
 
     return (
@@ -7,21 +28,7 @@ const Projects = () => {
                  style={{border: 'black 1px solid', borderRadius: "10px", background: "white"}}>
             <h1 style={{color: "#007ba7"}}>Projects</h1>
             <div className="row">
-                <div className="col-12 col-md-6 mt-5 mb-5">
-                    <h2>Pokemonline</h2>
-                    <div className={'pt-3'}>Gen 1 Pokemon MMORPG powered by WebGL and socket.io</div>
-                    <div className={'pt-3'}><a href="https://github.com/lrminer/pokemonline">GitHub Repo</a>
-                    </div>
-                </div>
-                <div className="col-12 col-md-6 mt-5 mb-5">
-                    <h2>Daily Translator</h2>
-                    <div className={'pt-3'}>Full Catholic Bible with Spanish translations using swiping
-                        gestures. Pulls up a new chapter of bible each day.
-                    </div>
-                    <div className={'pt-3'}><a href="https://github.com/lrminer/daily-translator">GitHub
-                        Repo</a></div>
-                </div>
-
+                {projects.map(project => <Project {...project} />)}
             </div>
         </section>
 
