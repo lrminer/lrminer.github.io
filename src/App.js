2b6904c1b321e9757cb9ff9dfdb7b2b6b1e2c684
Logan Miner <49819843+lrminer@users.noreply.github.com>
2020-12-10 17:29:27 -0500

    theme saves locally for persistence

--- src/App.js
+++ src/App.js
@@ -6,22 +6,20 @@ import Main from "./components/Main";
 import {ThemeProvider} from 'styled-components';
 import {lightTheme, darkTheme} from './theme';
 import {GlobalStyles} from './global';
+import { useDarkMode } from './useDarkMode';
+
 
 function App() {
-    const [theme, setTheme] = useState('light');
+    const [theme, toggleTheme, componentMounted] = useDarkMode();
 
-    const toggleTheme = () => {
-        // if the theme is not light, then set it to dark
-        if (theme === 'light') {
-            setTheme('dark');
-            // otherwise, it should be light
-        } else {
-            setTheme('light');
-        }
-    }
+    const themeMode = theme === 'light' ? lightTheme : darkTheme;
+
+    if (!componentMounted) {
+        return <div />
+    };
 
     return (
-        <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+        <ThemeProvider theme={themeMode}>
             <GlobalStyles />
             <div  className={'pt-5'}>
                 <div id="topOfPage"></div>
