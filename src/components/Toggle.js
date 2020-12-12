62ce72a06af8758e1faddfa78539c5e5312677e4
Logan Miner <49819843+lrminer@users.noreply.github.com>
2020-12-12 09:33:33 -0500

    slides moon/sun symbols on toggle

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -4,9 +4,9 @@ import ToggleContainer from "./Toggle.styled";
 
 const Toggle = ({theme, toggleTheme}) => {
     return (
-        <ToggleContainer className={"ml-3 pl-3"} onClick={toggleTheme}>
-            {theme === 'dark' && <i style={{color: "white"}} className="fas fa-sun fa-2x"></i>}
-            {theme === "light" && <i className="fas fa-moon fa-2x"></i>}
+        <ToggleContainer className={"ml-3"} onClick={toggleTheme}>
+            {theme === 'dark' && <i style={{color: "white"}} className="fas fa-sun fa-2x ml-3"></i>}
+            {theme === "light" && <i className="fas fa-moon fa-2x mr-3"></i>}
         </ToggleContainer>
     );
 };
--- src/components/Toggle.styled.js
+++ src/components/Toggle.styled.js
@@ -5,18 +5,13 @@ const ToggleContainer = styled.button`
   border: 2px solid ${({ theme }) => theme.toggleBorder};
   border-radius: 25px;
   cursor: pointer;
-  //display: flex;
   font-size: 0.5rem;
-  //justify-content: space-between;
-  //margin: 0 auto;
-  //overflow: hidden;
-  //padding: 0.5rem;
-  //position: relative;
-  //top: 50px;
   width: 3rem;
   max-height: 2rem;
-  //height: 4rem;
 
+  &:focus {
+    outline: none;
+  }
   
 `;
 export default ToggleContainer;
